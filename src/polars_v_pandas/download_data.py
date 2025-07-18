ff620084b0dbea3bf1339d5ce2c0ec32c6e3caa7
Sitt Guruvanich <aekazitt@gmail.com>
2025-07-18 22:11:12 +0700

    chore(deps): add matplotlib & etch compare script

--- src/polars_v_pandas/download_data.py
+++ src/polars_v_pandas/download_data.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python3
+# coding:utf-8
+# Copyright (C) 2024-2025 All rights reserved.
+# FILENAME:    ~~/src/polars_v_pandas/download_data.py
+# VERSION:     0.0.1
+# CREATED:     2025-07-18 21:27
+# AUTHOR:      Sitt Guruvanich <aekazitt+github@gmail.com>
+# DESCRIPTION:
+#
+# HISTORY:
+# *************************************************************
 
-import pandas as pd
-import yfinance as yf
+### Third-party packages ###
+from pandas import DataFrame, concat
+import yfinance as download
 
 
-tickers = ["tsla", "msft"]
-
-ticker_list = [pd.DataFrame(yf.download(ticker, start="2024-01-01", end="2024-02-26")) for ticker in tickers]
-pd.concat(ticker_list).to_csv("data.csv")
-
+if __name__ == "__main__":
+  tickers: tuple[str, ...] = ("tsla", "msft")
+  data: list[DataFrame] = [
+    DataFrame(download(ticker, start="2024-01-01", end="2024-02-26")) for ticker in tickers
+  ]
+  concat(data).to_csv("data.csv")
